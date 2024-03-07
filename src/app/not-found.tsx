fedf2223ca40f06a0d33770ef47c431954d35bb4
JasonCrk <emerzonkolki@gmail.com>
2024-03-07 10:07:30 -0500

    feat: margin bottom to not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@
 
 export default function NotFoundPage() {
   return (
-    <main className='flex h-screen flex-col items-center justify-center gap-2'>
+    <main className='flex h-screen flex-col items-center justify-center gap-2 pb-48'>
       <h1 className='text-7xl font-bold'>404</h1>
       <h2 className='text-3xl'>La pagina no existe</h2>
       <p className=''>Vuelva al inicio dando click al logo</p>
