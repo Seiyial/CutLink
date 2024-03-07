4ca81165f6d1acbfa51f20143cd603d0b929554f
JasonCrk <emerzonkolki@gmail.com>
2024-03-06 22:22:46 -0500

    feat: get stated page

---
+++ src/app/get-started/page.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+
+import SignInWithGitHubButton from '@/components/SignInWithGitHubButton'
+
+export default function GetStatedPage() {
+  return (
+    <section className='flex h-screen flex-col items-center justify-center gap-3 pb-48'>
+      <h1 className='w-1/2 text-balance text-center text-6xl font-bold leading-9'>
+        START NOW 🤩!
+      </h1>
+      <h2 className='text-balance text-xl'>
+        Registering with{' '}
+        <Link
+          href='https://github.com/'
+          target='_blank'
+          className='font-semibold text-gray-400 underline decoration-gray-400 underline-offset-4 transition-colors ease-out hover:text-gray-900 hover:decoration-gray-900'
+        >
+          GitHub
+        </Link>
+      </h2>
+      <SignInWithGitHubButton />
+    </section>
+  )
+}
