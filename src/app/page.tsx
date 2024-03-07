9d9c78d72dbb77b8445b1904acfef5ea508f0c5c
JasonCrk <emerzonkolki@gmail.com>
2024-03-07 18:27:47 -0500

    feat(app/page.tsx): add entrance animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,18 +14,21 @@ export default function Home() {
     <main className='flex h-screen flex-col items-center justify-center gap-1 pb-48'>
       <LandingNavbar />
 
-      <h1 className='flex items-center gap-2 text-8xl'>
+      <h1 className='animate-fade-up flex items-center gap-2 text-8xl'>
         <MdLink fontSize='6.2rem' />
         <span className='font-bold'>CutLink</span>
         <LiaCutSolid fontSize='6.2rem' />
       </h1>
 
-      <p className='mb-2 w-2/6 text-center text-gray-600'>
+      <p className='animate-fade-up animate-delay-150 mb-2 w-2/6 text-center text-gray-600'>
         Transform your long and complicated links into something more
         presentable and easy to remember
       </p>
 
-      <Link href='/getting_started'>
+      <Link
+        className='animate-fade-up animate-delay-300'
+        href='/getting_started'
+      >
         <Button>Get Started</Button>
       </Link>
     </main>
