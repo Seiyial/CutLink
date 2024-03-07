9e5f3eede6ad21776113bf42ef8d6eb9e35c8492
JasonCrk <emerzonkolki@gmail.com>
2024-03-06 22:22:36 -0500

    feat: not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFoundPage() {
+  return (
+    <main className='flex h-screen flex-col items-center justify-center gap-2'>
+      <h1 className='text-7xl font-bold'>404</h1>
+      <h2 className='text-3xl'>La pagina no existe</h2>
+      <p className=''>Vuelva al inicio dando click al logo</p>
+    </main>
+  )
+}
