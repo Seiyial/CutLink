e5d5912300008b81158d6535cd54c3e0ca119482
JasonCrk <emerzonkolki@gmail.com>
2024-03-07 10:09:58 -0500

    fix(not-found.tsx): Go to homepage link

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,17 @@
 'use client'
 
+import Link from 'next/link'
+
+import Button from '@/components/Button'
+
 export default function NotFoundPage() {
   return (
     <main className='flex h-screen flex-col items-center justify-center gap-2 pb-48'>
       <h1 className='text-7xl font-bold'>404</h1>
       <h2 className='text-3xl'>La pagina no existe</h2>
-      <p className=''>Vuelva al inicio dando click al logo</p>
+      <Link href='/'>
+        <Button>Go to homepage</Button>
+      </Link>
     </main>
   )
 }
