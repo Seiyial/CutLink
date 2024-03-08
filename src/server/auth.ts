db4bb6be7991f00fc570ad677c1bfc724434ca1a
JasonCrk <emerzonkolki@gmail.com>
2024-03-07 22:06:08 -0500

    fix(auth.ts): image type and

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,34 +1,30 @@
-import { PrismaAdapter } from '@auth/prisma-adapter'
 import {
   getServerSession,
   type DefaultSession,
   type NextAuthOptions,
 } from 'next-auth'
-import { type Adapter } from 'next-auth/adapters'
+import type { Adapter } from 'next-auth/adapters'
 import GithubProvider from 'next-auth/providers/github'
 
-import { env } from '@/env'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+
 import { db } from '@/server/db'
 
-/**
- * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
- *
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- */
+import { env } from '@/env'
+
 declare module 'next-auth' {
   interface Session extends DefaultSession {
     user: {
       id: string
       name: string
-      image: string | null
+      image: string
     } & DefaultSession['user']
   }
 
   interface User {
     id: string
     name: string
-    image: string | null
+    image: string
   }
 }
 
@@ -53,15 +49,6 @@ export const authOptions: NextAuthOptions = {
       clientId: env.GITHUB_CLIENT_ID,
       clientSecret: env.GITHUB_CLIENT_SECRET,
     }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
   ],
 }
 
