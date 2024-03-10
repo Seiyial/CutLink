e6dc079316c707a706f2a5c76a18f16a106ba058
JasonCrk <emerzonkolki@gmail.com>
2024-03-09 22:01:37 -0500

    feat(next.config.js): enable GitHub avatar image url

--- next.config.js
+++ next.config.js
@@ -7,6 +7,16 @@ await import('./src/env.js')
 /** @type {import("next").NextConfig} */
 const config = {
   output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/u/**',
+      },
+    ],
+  },
 }
 
 export default config
