a048c7ae32db31aaa1be34b343b3dc129eb546e2
JasonCrk <emerzonkolki@gmail.com>
2024-03-23 17:33:25 -0500

    feat(ShortLinkCard.tsx): copy short link url

--- src/components/ShortLinkCard.tsx
+++ src/components/ShortLinkCard.tsx
@@ -1,6 +1,9 @@
+import Tooltip from '@/components/Tooltip'
+
 import { getTimeAgo } from '@/lib/timeAgo'
 
 import { TbWorld } from 'react-icons/tb'
+import { IoCopyOutline } from 'react-icons/io5'
 
 interface Props {
   code: string
@@ -11,13 +14,29 @@ interface Props {
 }
 
 const ShortLinkCard = ({ code, alias, originalUrl, createdAt }: Props) => {
+  const name = alias === '' || alias === null ? code : alias
+  const shortLinkUrl = `http://localhost:3000/${name}`
+
+  const handleCopyShortLinkUrl = async () => {
+    await navigator.clipboard.writeText(shortLinkUrl)
+  }
+
   return (
     <article className='rounded-lg bg-gradient-to-b from-gray-100 to-transparent p-3.5'>
       <h6 className='flex'>
         <TbWorld fontSize='1.7rem' className='self-start' />
 
         <span className='text-xl font-semibold'>
-          <span>/{!!alias ? alias : code}</span>{' '}
+          <span>/{name}</span>{' '}
+          <Tooltip
+            className='inline-block align-middle leading-none'
+            title='Copy'
+          >
+            <IoCopyOutline
+              className='cursor-pointer text-xl'
+              onClick={handleCopyShortLinkUrl}
+            />
+          </Tooltip>
         </span>
       </h6>
 
