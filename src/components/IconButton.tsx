8d62c2ea84ff64ea4bef7b085c58b1f5e0be4aa1
JasonCrk <emerzonkolki@gmail.com>
2024-03-23 22:49:32 -0500

    refactor(IconButton.tsx): add forwardRef to component

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,22 +1,27 @@
-import type { HTMLAttributes, ReactNode } from 'react'
+import {
+  forwardRef,
+  type HTMLAttributes,
+  type ReactNode,
+  type ForwardedRef,
+} from 'react'
 
 interface Props extends HTMLAttributes<HTMLButtonElement> {
   children: ReactNode
 }
 
-const IconButton = ({
-  children,
-  className: extraClasses,
-  ...btnProps
-}: Props) => {
+const IconButton = forwardRef(function iconButton(
+  { children, className: extraClasses, ...btnProps }: Props,
+  ref: ForwardedRef<any>,
+) {
   return (
     <button
+      ref={ref}
       className={`grid place-content-center rounded p-2 ${extraClasses}`}
       {...btnProps}
     >
       {children}
     </button>
   )
-}
+})
 
 export default IconButton
