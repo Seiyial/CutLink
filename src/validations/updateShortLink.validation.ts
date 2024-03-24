3c3e5f139f83a349a5c76f4e2876ddac2949df6c
JasonCrk <emerzonkolki@gmail.com>
2024-03-24 17:58:10 -0500

    fix(validations): add shortLink alias validations

--- src/validations/updateShortLink.validation.ts
+++ src/validations/updateShortLink.validation.ts
@@ -5,7 +5,14 @@ export const updateShortLinkValidation = z.object({
     .string()
     .max(30, 'Maximum 30 characters')
     .nullable()
-    .transform(alias => (alias === '' ? null : alias)),
+    .transform(alias => (alias === '' ? null : alias))
+    .refine(alias => (!!alias ? alias.indexOf('/') < 0 : true), {
+      message: 'The alias cannot contain a "/"',
+    })
+    .refine(alias => (!!alias ? alias !== 'dashboard' : true), {
+      message: 'The alias cannot be equal to "dashboard"',
+    })
+    .transform(alias => (alias !== null ? alias.replaceAll(' ', '-') : null)),
   description: z
     .string()
     .nullable()
